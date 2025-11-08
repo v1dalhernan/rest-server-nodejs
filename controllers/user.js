d17b6d18380208609514b8136ed80b7ee831dd2e
Jhonathan Vidal <jhonathanvidalvv@gmail.com>
2025-11-08 16:18:28 -0500

    reescritura

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,13 @@ const { response, request } = require('express');
 
 const userGet = (req = request, res = response) => {
 
-    const { q, nombre = "no name", apikey, page = 1, limit=10 } = req.query;
+    const { 
+        q, 
+        nombre = "no name", 
+        apikey, 
+        page = 1, 
+        limit = 10 
+    } = req.query;
 
 
     res.status(200).json({
